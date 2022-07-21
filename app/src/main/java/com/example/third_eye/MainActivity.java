f833de8ab6d7fe23a54ce82ee2810539e4464409
nayemshaon34 <nayemshaon34@gmail.com>
2022-07-22 00:18:32 +0600

    initial commit

---
+++ app/src/main/java/com/example/third_eye/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.third_eye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
